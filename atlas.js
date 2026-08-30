47cb6e46f5779731e4a75d59bc74ac2575dfd519
Panda-852 <paulrechten@gmail.com>
2026-08-30 19:05:54 +0800

    Refine Atlas interface

--- atlas.js
+++ atlas.js
@@ -3243,7 +3243,7 @@ map.on('load', () => {
 
     map.addSource('mtr',{
         type:'geojson',
-        data:'https://pub-c831f6efbc4341068a1653dcf6c592b9.r2.dev/mtr/MTR_Lines_TEST.geojson'
+        data:'https://pub-c831f6efbc4341068a1653dcf6c592b9.r2.dev/mtr/https://pub-c831f6efbc4341068a1653dcf6c592b9.r2.dev/mtr/MTR_Lines_TEST.geojson'
     });
 
 
